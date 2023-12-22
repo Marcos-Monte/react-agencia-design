fe655a9674027ecff6e9720045f89322e9733f1f
Burunga <monte_marcos@hotmail.com>
2023-12-22 20:46:00 -0300

    Criação do componente 'SecaoBanner'

--- src/Componentes/SecaoBanner/index.js
+++ src/Componentes/SecaoBanner/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
+import ImagemBanner from '../../assets/banner.png';
+
 import './style.css';
 
 const SecaoBanner = () => {
     return (
-        <div>
-            SecaoBanner
-        </div>
+        <section className="banner">
+            <img src={ImagemBanner} alt="Deginer" />
+            <div className="banner-conteudo">
+                <p className="banner-conteudo-estilizado">branding / ui / ux / tecnologia</p>
+                <h1 className="banner-conteudo-titulo">Agencia de Branding <span className="conteudo-destaque">e design digital</span></h1>
+            </div>
+        </section>
     )
 }
 
