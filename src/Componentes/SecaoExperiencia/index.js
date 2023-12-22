780de7fc55b95c2572a5fb7b2d2b72123a257514
Burunga <monte_marcos@hotmail.com>
2023-12-22 20:50:49 -0300

    Criação do componente 'SecaoExperiencia'

--- src/Componentes/SecaoExperiencia/index.js
+++ src/Componentes/SecaoExperiencia/index.js
@@ -4,9 +4,18 @@ import './style.css';
 
 const SecaoExperiencia = () => {
     return (
-        <div>
-            SecaoExperiencia
-        </div>
+        <section className="experiencia">
+
+            <h2 className="experiencia-titulo">Experiências De Trabalho</h2>
+            <p className="experiencia-paragrafo">Há mais de 10 anos no mercado de Branding, Desing Gráfico, Criação de Sites de Marketing Digital, nos empenhamos diariamente para entragar resultados que tragam impacto aos nossos clientes.</p>
+
+            <div className="experiencia-container">
+                <div>Card</div>
+                <div>Card</div>
+                <div>Card</div>
+            </div>
+
+        </section>
     )
 }
 
