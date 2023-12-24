3bf1faaf4d1ded571e87e60d528fd936648fbf29
Burunga <monte_marcos@hotmail.com>
2023-12-24 11:17:04 -0300

    Estilização de destaques: Titulo da SecaoExperiencias, Card (hover)

--- src/Componentes/SecaoExperiencia/index.js
+++ src/Componentes/SecaoExperiencia/index.js
@@ -10,7 +10,12 @@ const SecaoExperiencia = () => {
 
             <div className="largura-tela">
 
-                <h2 className="experiencia-titulo">Experiências De Trabalho</h2>
+                <div className="titulo-destaque">
+
+                    <h2 className="experiencia-titulo">Experiências De Trabalho</h2>
+
+                </div>
+
                 <p className="experiencia-paragrafo">Há mais de 10 anos no mercado de Branding, Desing Gráfico, Criação de Sites de Marketing Digital, nos empenhamos diariamente para entragar resultados que tragam impacto aos nossos clientes.</p>
 
                 <div className="experiencia-container">
